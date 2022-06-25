1c347f61643473e37c3adc7de54d8b93386e1926
李康伟 <kwliIanOpm@gmail.com>
2022-06-25 17:22:29 +0800

    copy

--- chapter4/copy.c
+++ chapter4/copy.c
@@ -9,15 +9,33 @@
 #define BUF_SIZE  1024
 #endif
 
+char buf[BUF_SIZE];
+
 int main(int argc, char* argv[]) {
-    int input_fd,outputFd,open_flag;
-    mode_t file_perms;
-    ssize_t num_read;
     if (argc != 3 ){
         fprintf(stderr,"usage: copy src target");
         exit(1);
     }
-
-
+    int  input_fd = open(argv[1],O_RDONLY);
+    if (input_fd == -1){
+        fprintf(stderr,"can not open file: %s",strerror(errno));
+        exit(1);
+    }
+    mode_t perm = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
+    int open_flags = O_CREAT|O_WRONLY|O_TRUNC;
+    int output_fd = open(argv[2],open_flags,perm);
+    if (output_fd == -1){
+        fprintf(stderr,"can not open file: %s",strerror(errno));
+        exit(1);
+    }
+    ssize_t num_read;
+    while ((num_read = read(input_fd,buf,BUF_SIZE)) > 0){
+        if (write(output_fd,buf,num_read)  != num_read){
+            fprintf(stderr,"can not write file: %s",strerror(errno));
+            exit(1);
+        }
+    }
+    close(input_fd);
+    close(output_fd);
     return 0;
 }
