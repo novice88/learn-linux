212897c2aef7ca4eccd0cab00211f70aed22bdab
李康伟 <kwliIanOpm@gmail.com>
2022-06-26 18:22:51 +0800

    copy

--- chapter4/copy.c
+++ chapter4/copy.c
@@ -6,32 +6,32 @@
 #include <unistd.h>
 
 #ifndef BUF_SIZE
-#define BUF_SIZE  1024
+#define BUF_SIZE  10240
 #endif
 
 char buf[BUF_SIZE];
 
 int main(int argc, char* argv[]) {
     if (argc != 3 ){
-        fprintf(stderr,"usage: copy src target");
+        fprintf(stderr,"usage: copy src target\n");
         exit(1);
     }
     int  input_fd = open(argv[1],O_RDONLY);
     if (input_fd == -1){
-        fprintf(stderr,"can not open file: %s",strerror(errno));
+        fprintf(stderr,"can not open file: %s\n",strerror(errno));
         exit(1);
     }
     mode_t perm = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
     int open_flags = O_CREAT|O_WRONLY|O_TRUNC;
     int output_fd = open(argv[2],open_flags,perm);
     if (output_fd == -1){
-        fprintf(stderr,"can not open file: %s",strerror(errno));
+        fprintf(stderr,"can not open file: %s\n",strerror(errno));
         exit(1);
     }
     ssize_t num_read;
     while ((num_read = read(input_fd,buf,BUF_SIZE)) > 0){
         if (write(output_fd,buf,num_read)  != num_read){
-            fprintf(stderr,"can not write file: %s",strerror(errno));
+            fprintf(stderr,"can not write file: %s\n",strerror(errno));
             exit(1);
         }
     }
